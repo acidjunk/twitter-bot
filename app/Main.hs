eba93e27726cb15e59de39c6c2b040462dfe0eae
Luc Tielen <luc.tielen@gmail.com>
2022-02-02 15:34:04 +0100

    Add searchForKeywords

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,19 @@
 module Main where
 
 import qualified Bot.Twitter as Twitter
+import Web.Tweet
+
+configFilePath :: FilePath
+configFilePath = "/Users/acidjunk/.cred.toml"
 
 main :: IO ()
 main = do
-  let keywords = [Twitter.Keyword "music", Twitter.Keyword "jazz"]
-  authors <- Twitter.runTwitter ".cred.toml" $ Twitter.searchForKeywords keywords
-  print authors
+  let keywords = Twitter.SearchTerm "music%20jazz"
+  (tweets, results) <- Twitter.runTwitter configFilePath $ do
+    userTweets <- Twitter.searchTweetsForUser (Twitter.User "acidjunk")
+    searchResults <- Twitter.searchForKeywords keywords
+    pure (userTweets, searchResults)
+
+  let parsedTweets = map (\t -> _screenName t <> ":" <> _text t) tweets
+  print parsedTweets
 
--- src/Bot/Twitter.hs
+++ src/Bot/Twitter.hs
@@ -1,15 +1,18 @@
-{-# LANGUAGE DeriveFunctor, DerivingVia #-}
+{-# LANGUAGE OverloadedStrings, DeriveFunctor, DerivingVia #-}
 
 module Bot.Twitter
-  ( Author(..)
-  , Keyword(..)
+  ( User(..)
+  , SearchTerm(..)
   , TwitterM
   , runTwitter
   , searchForKeywords
-  , getLastTweets
+  , searchTweetsForUser
   ) where
 
+import Data.Maybe (fromJust)
 import qualified Data.Text as T
+import qualified Data.Aeson as A
+import Data.Aeson ((.:))
 import Control.Monad.Reader
 import Web.Tweet
 import Web.Tweet.API
@@ -21,26 +24,43 @@ newtype TwitterM a
   deriving (Applicative, Monad, MonadReader FilePath, MonadIO)
   via ReaderT FilePath IO
 
-
 runTwitter :: FilePath -> TwitterM a -> IO a
 runTwitter credentialsPath (TwitterM action) =
   runReaderT action credentialsPath
 
-newtype Keyword = Keyword T.Text
+
+newtype SearchTerm = SearchTerm T.Text
   deriving (Eq, Show)
 
-newtype Author = Author T.Text
+newtype User = User T.Text
   deriving (Eq, Show)
 
-searchForKeywords :: [Keyword] -> TwitterM [Author]
-searchForKeywords keywords = do
+data SearchResult
+  = SearchResult
+  { _tweetText :: T.Text
+  , _userName :: User
+  , _description :: T.Text
+  }
+
+instance A.FromJSON SearchResult where
+  parseJSON = A.withObject "" $ \value -> do
+    tweetText <- value .: "text"
+    userInfo <- value .: "user"
+    screenName <- userInfo .: "screen_name"
+    description <- userInfo .: "description"
+    pure $ SearchResult tweetText (User screenName) description
+
+
+searchForKeywords :: SearchTerm -> TwitterM [SearchResult]
+searchForKeywords (SearchTerm keywords) = do
   credentialsPath <- ask
-  let userName = "acidjunk"
-      tweetCount = 42
-  tweets <- liftIO $ getAll userName (Just tweetCount) credentialsPath
-  pure $ map (\t -> Author $ T.pack $ _screenName t) tweets
+  let query = "?q=" <> T.unpack keywords
+  results <- liftIO $ searchRaw query credentialsPath
+  -- TODO: use Either iso fromJust?
+  pure $ fromJust $ A.decode results
 
-getLastTweets :: Author -> Int -> TwitterM [Tweet]
-getLastTweets author count = undefined
+searchTweetsForUser :: User -> TwitterM [TweetEntity]
+searchTweetsForUser (User userName) = do
+  credentialsPath <- ask
+  liftIO $ getAll (T.unpack userName) Nothing credentialsPath
 
--- TODO: function for searching if user has certain keywords in last X tweets (in other module?)
