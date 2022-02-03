996c94fb108b44ebd8d573403a2d249af5e3378b
René Dohmen <acidjunk@gmail.com>
2022-02-03 01:24:55 +0100

    Cleaned up a bit

--- app/Main.hs
+++ app/Main.hs
@@ -3,20 +3,32 @@
 module Main where
 
 import qualified Bot.Twitter as Twitter
+import qualified Data.Text as T
 import Web.Tweet
 
 configFilePath :: FilePath
 configFilePath = "/Users/acidjunk/.cred.toml"
 
+twitterUser :: T.Text 
+twitterUser = "acidjunk"
+
+stringFromChar :: Char -> String
+stringFromChar x = [x]
+
 main :: IO ()
 main = do
+  -- Execute a search and display the account + bio
   let keywords = Twitter.SearchTerm "music%20jazz"
-  (tweets, results) <- Twitter.runTwitter configFilePath $ do
-    userTweets <- Twitter.searchTweetsForUser (Twitter.User "acidjunk")
+  searchResults <- Twitter.runTwitter configFilePath $ do
     searchResults <- Twitter.searchForKeywords keywords
-    pure (userTweets, searchResults)
+    pure searchResults
 
-  let parsedTweets = map (\t -> _screenName t <> ":" <> _text t) tweets
-  print parsedTweets
-  print results
+  print searchResults
 
+  -- FETCH ALL USERTWEETS and dump them in the console
+  -- userTweets <- Twitter.runTwitter configFilePath $ do
+  --   userTweets <- Twitter.searchTweetsForUser (Twitter.User twitterUser)
+  --   pure userTweets
+
+  -- let parsedTweets = map (\t -> _screenName t <> ":" <> _text t) userTweets
+  -- mapM_ putStrLn parsedTweets
